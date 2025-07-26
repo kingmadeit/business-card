b561fd4968ba9b1ee43a800fe0cd90f95b6c6ae8
King <kingsley.adarkwah@gmail.com>
2025-07-26 03:03:16 +0000

    add content for contact links page

--- src/components/ContactLinks.tsx
+++ src/components/ContactLinks.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { TbBrandLinkedin } from "react-icons/tb";
+import { RiTwitterXLine } from "react-icons/ri";
+import { VscGithub } from "react-icons/vsc";
+import { HiOutlineMail } from "react-icons/hi";
 
 const ContactLinks = () => {
   return (
-    <div>ContactLinks</div>
+    <div className="flex space-x-4 justify-center items-center text-white/60 text-2xl">
+      <a href="https://github.com/kingmadeit" target="_blank" rel="noopener noreferrer">
+        <VscGithub className="hover:text-black transition-colors" />
+      </a>
+      <a href="https://linkedin.com/in/kingsleyadarkwah" target="_blank" rel="noopener noreferrer">
+        <TbBrandLinkedin className="hover:text-blue-400 transition-colors" />
+      </a>
+       <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <RiTwitterXLine className="hover:text-black transition-colors" />
+      </a>
+      <a href="mailto:kingsley.adarkwah@gmail.com" target="_blank" rel="noopener noreferrer">
+        <HiOutlineMail className="hover:text-green-400 transition-colors" />
+      </a>
+    </div>
+
   )
 }
 
--- src/components/Qrcode.tsx
+++ src/components/Qrcode.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { QRCodeSVG } from 'qrcode.react'
 const Qrcode = () => {
   return (
-    <section className='flex w-[130px] rounded-sm justify-center items-center border border-white/50'>
-      <QRCodeSVG  className='qrcode' opacity='0.7' value='https://laris-mdus.vercel.app'/>
+    <section className='flex w-[128px] h-[128px] mt-2 mb-4 rounded-sm justify-center items-center border border-white/50'>
+      <QRCodeSVG  className='qrcode' opacity='0.7' value='https://linking.vercel.app'/>
     </section>
   )
 }
