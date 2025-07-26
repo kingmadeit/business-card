c13e37d842325e9a2e39106dfb68bf150614ecfb
King <kingsley.adarkwah@gmail.com>
2025-07-26 01:22:42 +0000

    add content for qrcode component

--- src/components/Qrcode.tsx
+++ src/components/Qrcode.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { QRCodeSVG } from 'qrcode.react'
 const Qrcode = () => {
   return (
-    <div>Qrcode</div>
+    <section className='flex w-[130px] rounded-sm justify-center items-center border border-white/50'>
+      <QRCodeSVG  className='qrcode' opacity='0.7' value='https://laris-mdus.vercel.app'/>
+    </section>
   )
 }
 
